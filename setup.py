7b6d42bf83f1170cb46a9e4c892e2b8a37100b73
Yui Koi <88405792+Yui-Koi@users.noreply.github.com>
2025-04-12 21:29:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,14 @@ from setuptools import setup
 import re
 
 with open('discord/ext/voice_recv/__init__.py') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)  # type: ignore
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 if not version:
     raise RuntimeError('version is not set')
 
 if version.endswith(('a', 'b', 'rc')):
-    # append version identifier based on commit count
     try:
         import subprocess
-
         p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         if out:
@@ -33,16 +31,16 @@ extras_require = {
 setup(
     name='discord-ext-voice_recv',
     author='Imayhaveborkedit',
-    url='https://github.com/imayhaveborkedit/discord-ext-voice-recv',
+    url='https://github.com/Yui-Koi/discord-ext-voice-recv',
     version=version,
     packages=['discord.ext.voice_recv', 'discord.ext.voice_recv.extras'],
     license='MIT',
-    description='Experimental voice receive extension for discord.py',
+    description='Experimental voice receive extension for discord.py-self',
     long_description=readme,
     long_description_content_type='text/markdown',
     include_package_data=True,
     python_requires='>=3.8',
-    install_requires=['discord.py[voice]>=2.5'],
+    install_requires=['discord.py-self[voice]'],
     extras_require=extras_require,
     zip_safe=False,
     classifiers=[
